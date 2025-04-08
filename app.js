8376b358d863c079dd99311c27491e45a5387a23
AlexisTISSIER <tissieralexis@outlook.fr>
2025-04-08 16:19:24 +0200

    Ajout intégrer les modules formidable ou multer afin d’ajouter un champ d’envoi de fichier à votre formulaire précédent

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}${ext}`);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -18,15 +32,19 @@ app.get('/contact', (req, res) => {
     res.render('contact', { email: 'contact@exemple.com', telephone: '0123456789', nom: null, message: null });
   });
   
-  app.post('/contact', (req, res) => {
+  app.post('/contact', upload.single('fichier'), (req, res) => {
     const { nom, message } = req.body;
+    const fichier = req.file;
+  
     res.render('contact', {
       email: 'contact@exemple.com',
       telephone: '0123456789',
       nom,
-      message
+      message,
+      fichier: fichier ? fichier.originalname : null
     });
   });
+  
 
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
